481c52dd81d960a1761ce322b90f4b2fab4a25b0
Digital Nexus <ricardo.osoriogomes@outlook.com>
2024-05-15 17:06:17 +0200

    Adjustements

--- static/main.js
+++ static/main.js
@@ -43,24 +43,17 @@ function erase() {
 
 // ---------------------- Fonction pour le traitement de la navbar ------------------------
 
-// Sélectionnez la barre de navigation
 const navBar = document.querySelector('.top-header');
 
-// Ajoutez un écouteur d'événement de défilement à la fenêtre
 window.addEventListener('scroll', () => {
-    // Récupérez la position de défilement de la page
     const scrollPosition = window.scrollY;
     
-    // Récupérez la position de la section suivante (par exemple, la section des compétences)
     const skillsSection = document.querySelector('.arrow');
     const skillsSectionPosition = skillsSection.offsetTop;
     
-    // Vérifiez si la position de défilement dépasse la position de la section des compétences
     if (scrollPosition >= skillsSectionPosition) {
-        // Si oui, ajoutez la classe 'scrolled' à la barre de navigation
         navBar.classList.add('scrolled');
     } else {
-        // Sinon, retirez la classe 'scrolled'
         navBar.classList.remove('scrolled');
     }
 });
@@ -114,18 +107,13 @@ const contentContainer = document.querySelector(".content-container-2");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  // Ajoutez ou supprimez la classe "toggle" sur l'élément contentContainer
   contentContainer.classList.toggle("toggle");
   
-  // Obtenez la hauteur du contenu réel
   const contentHeight = contentContainer.scrollHeight;
   
-  // Vérifiez si la classe "toggle" est maintenant présente sur l'élément
   if (contentContainer.classList.contains("toggle")) {
-    // Si oui, définissez la hauteur maximale sur la hauteur du contenu réel
     contentContainer.style.maxHeight = contentHeight + "px";
   } else {
-    // Sinon, définissez la hauteur maximale sur 0 pour masquer le contenu
     contentContainer.style.maxHeight = 0;
   }
 });
@@ -135,24 +123,19 @@ btn.addEventListener("click", () => {
 
 // --------------------- Formulaire de contact -------------------------------
 
-// Obtenez le bouton de contact et le pop-up
 var contactBtn = document.getElementById("contact-btn");
 var contactModal = document.getElementById("contact-modal");
 
-// Obtenez le bouton de fermeture
 var closeBtn = document.getElementsByClassName("close")[0];
 
-// Lorsque l'utilisateur clique sur le bouton "Contactez-moi", affichez le pop-up
 contactBtn.onclick = function() {
   contactModal.style.display = "block";
 }
 
-// Lorsque l'utilisateur clique sur le bouton de fermeture (x), masquez le pop-up
 closeBtn.onclick = function() {
   contactModal.style.display = "none";
 }
 
-// Lorsque l'utilisateur clique en dehors du pop-up, masquez-le également
 window.onclick = function(event) {
   if (event.target == contactModal) {
     contactModal.style.display = "none";
